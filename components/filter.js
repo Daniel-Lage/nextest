7385706a9c2fcc70ccb4f8df8af70a08988bb97c
= <=>
2023-04-08 17:57:01 -0300

    cant clear empty filter

--- components/filter.js
+++ components/filter.js
@@ -11,14 +11,16 @@ export default function Filter({ filter, setFilter }) {
         placeholder={"Search"}
         onKeyDown={(e) => e.key === "Escape" && setFilter("")}
       />
-      <div
-        className="filterButton"
-        onClick={() => {
-          setFilter("");
-        }}
-      >
-        <Image src="/close.svg" alt="close" width={15} height={15} />
-      </div>
+      {filter && (
+        <div
+          className="filterButton"
+          onClick={() => {
+            setFilter("");
+          }}
+        >
+          <Image src="/close.svg" alt="close" width={15} height={15} />
+        </div>
+      )}
     </div>
   );
 }
